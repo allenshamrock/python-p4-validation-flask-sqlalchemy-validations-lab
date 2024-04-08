b96618dbebd109682caab9987f98e46fd43f39f5
Allen <allenshamrock37@gmail.com>
2024-04-09 00:44:39 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -12,6 +12,16 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self,key,name):
+        if not name:
+            raise ValueError('Failed name cannot be empty')
+        return name
+    @validates('phone_number')
+    def validate_phone_number(self,key,phone_number):
+        if phone_number and len(phone_number) !=10:
+            raise ValueError('Failed, phone number should have 10 characters')
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,7 +38,31 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError('Content must be at least 250 characters long')
+        return content
+    
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if summary and len(summary) > 250:
+            raise ValueError('Summary cannot exceed 250 characters')
+        return summary
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ["Fiction", "Non-Fiction"]:
+            raise ValueError('Category must be either Fiction or Non-Fiction')
+        return category
 
-
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait_keywords = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(keyword in title for keyword in clickbait_keywords):
+            raise ValueError(
+                'Title must contain one of the following: "Won\'t Believe", "Secret", "Top", "Guess"')
+        return title
+    
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
